a17e4930966ef2b7a4fad24be521ef52ca416116
li4nee <nishantpokharel9@gmail.com>
2025-08-26 11:38:42 +0545

    fix : clearing unnecessary things

--- Anonygle-Backend/src/migrations/1743438160383-user.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class User1743438160383 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
--- Anonygle-Backend/src/repo/repo.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-// import { UserRepository } from "./models/user.repo";
-
-@Module({
-  // providers: [UserRepository],
-  // exports: [UserRepository],
-})
-export class RepoModule {}
